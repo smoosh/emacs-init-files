d1df46beff6fe3aa3645f7acc174c93d7cc7aa56
Patrick Lynch <pnl.ognc@gmail.com>
2025-03-26 13:54:53 -0500

    Add files via upload

---
+++ Linux/custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ag-executable "c:/p/ag/ag.exe")
+ '(custom-safe-themes
+   '("b216e9b72dc8c2b702e4fcfd3c0af2d73c87eba46fd4db824ddb50863447d6a9" "841b6a0350ae5029d6410d27cc036b9f35d3bf657de1c08af0b7cbe3974d19ac" "515ebca406da3e759f073bf2e4c8a88f8e8979ad0fdaba65ebde2edafc3f928c" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "f079ef5189f9738cf5a2b4507bcaf83138ad22d9c9e32a537d61c9aae25502ef" "2ccdb4796d3238dd0794f7869750fb0e81fe4f9212f9528cfd4f41da0c78cf25" "263e3a9286c7ab0c4f57f5d537033c8a5943e69d142e747723181ab9b12a5855" "603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
+ '(elfeed-feeds
+   '("https://dailywire.com" "https://nightride.fm/?station=chillsynth") t)
+ '(exec-path
+   '("c:/p/findutils/bin" "C:/Windows/system32" "C:/Windows" "C:/Windows/System32/Wbem" "C:/Windows/System32/WindowsPowerShell/v1.0/" "C:/Windows/System32/OpenSSH/" "C:/Program Files/PuTTY/" "C:/Program Files/Common Files/ThinPrint/" "C:/Program Files/dotnet/" "C:/Program Files (x86)/Windows Kits/10/Windows Performance Toolkit/" "C:/Program Files (x86)/Windows Kits/10/Microsoft Application Virtualization/Sequencer/" "C:/Users/plynch/AppData/Local/Programs/Python/Launcher/" "C:/Users/plynch/AppData/Local/Microsoft/WindowsApps" "C:/Users/plynch/AppData/Local/Programs/Microsoft VS Code/bin" "C:/Users/plynch/AppData/Local/Pandoc/" "c:/Program Files/Emacs/emacs-29.3_2/libexec/emacs/29.3/x86_64-w64-mingw32" "c:/users/plynch/appdata/local/pandoc/" "c:/p/ag/"))
+ '(org-hide-emphasis-markers t)
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")))
+ '(package-selected-packages
+   '(@ beacon elfeed git markdown-mode org-bullets kaolin-themes gruvbox-theme eradio inkpot-theme ef-themes dracula-theme pdf-tools ox-pandoc dired-du powershell pandoc-mode pandoc ag symon-lingr symon spacemacs-theme term+ marginalia gnu-elpa-keyring-update vertico org-modern emms-player-mpv-jp-radios emms-player-spotify flycheck zenburn-theme timu-spacegrey-theme nerd-icons haskell-mode doom-themes dashboard)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
